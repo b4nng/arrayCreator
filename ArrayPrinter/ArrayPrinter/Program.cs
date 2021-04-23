d878947ab84af51482f432f41cd674d8589303ef
Jafar Mohammed Untar <JafarMohammed.Untar@thomsonreuters.com>
2021-04-23 11:29:58 -0300

    Simple program

---
+++ ArrayPrinter/ArrayPrinter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArrayPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arrayBegin = "[ ";
+            string arrayEnd = "]";
+            string arrayString = "";
+
+
+            while (true) {
+                string item = Console.ReadLine();
+                if (item.Equals("-1")) {
+                    break;
+                }
+                else {
+                    arrayString += "\""+item+"\""+", ";
+                }
+            }
+            Console.WriteLine(arrayBegin+arrayString+arrayEnd);
+        }
+    }
+}
